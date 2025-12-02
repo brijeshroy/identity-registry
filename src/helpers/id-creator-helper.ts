38e0dea4b34dc5f585daa2780381454e08b70986
brijeshroy <brijroy2502@gmail.com>
2025-12-02 07:21:27 +0530

    date undefined issue sorted

--- src/helpers/id-creator-helper.ts
+++ src/helpers/id-creator-helper.ts
@@ -2,7 +2,7 @@ import { UserCategory, UserCategoryKey } from "../constants/project-constants";
 
 export const idCreator = (userCategory:UserCategoryKey) : string =>{
     console.log('User is of type  ',userCategory)
-    const currentDate = new Date().toISOString().split("T")[0].replaceAll("-","")
+    const currentDate = new Date().toISOString().split("T")[0]!.replaceAll("-","")
     console.log("The Current Data is ",currentDate)
     const userId : string = UserCategory[userCategory].concat(`_${currentDate}`);
 
