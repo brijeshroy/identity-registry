1b4be10383ea2221808d17199d00a23da1081e3c
brijeshroy <brijroy2502@gmail.com>
2025-12-14 13:43:14 +0530

    id creation correction

--- src/helpers/id-creator-helper.ts
+++ src/helpers/id-creator-helper.ts
@@ -2,11 +2,14 @@ import { UserCategory, UserCategoryKey } from "../constants/project-constants";
 
 export const idCreator = (userCategory:UserCategoryKey) : string =>{
     console.log('User is of type  ',userCategory)
-    const currentDate = new Date().toISOString().split("T")[0]!.replaceAll("-","")
-    const currentTime = new Date().toISOString().split("T")[1]!.slice(0,7).replaceAll(":","")
+    const currentMoment = new Date().toISOString();
+    console.log(currentMoment)
+    const currentDate = currentMoment.split("T")[0]!.replaceAll("-","")
+    const currentTime = currentMoment.split("T")[1]!.slice(0,7).replaceAll(":","")
    
     console.log("The Current Data is ",currentDate)
-    const userId : string = UserCategory[userCategory].concat(`_${currentDate}_${currentTime}`);
+    console.log("the current time is",currentTime)
+    const userId : string = userCategory.concat(`_${currentDate}_${currentTime}`);
 
     console.log(`The user id is ${userId}`);
 
